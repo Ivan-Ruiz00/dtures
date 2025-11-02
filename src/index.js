ccbd80b44a0e18c350fadef0f369f8334b13c500
Ivan Ruiz <idruizbe@gmail.com>
2025-11-02 13:56:09 -0500

    Se hace la conexión con el puerto y el enlace con la autenticación

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const parser=require("body-parser");
+const express=require("express");
+const app=express();
+const port=3000;
+const authRoutes=require("./routes/authentication");
+const mongoose=require("mongoose");
+require("dotenv").config();
+
+app.use(parser.urlencoded({extended:false}));
+app.use(parser.json());
+
+app.use("/api",animalRoutes);
+app.use("/api",authRoutes);
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI).then(()=>console.log("Conexión exitosa")).catch((error)=>console.log(error));
+
+app.listen(
+    port,()=>{
+        console.log(`App de ejemplo escuchando el puerto ${port}`)
+    }
+)
