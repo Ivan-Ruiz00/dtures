b33082723fe7939562bde902af62820f285be889
Miguel Paez <luism.paezd@konradlorenz.edu.co>
2025-11-02 21:34:07 -0500

    Se agregan metodo para actualizar plan por id

--- src/routes/plan.js
+++ src/routes/plan.js
@@ -43,3 +43,26 @@ router.get(
         planSchema.find({pais:{$in:pais}}).then((data)=>res.json(data)).catch((error)=>res.json({message:error}));
     }
 );
+
+router.put(
+    "/plan/:id", (req, res) => {
+        const { id } = req.params;
+        const update = {};
+        for (const key in req.body) {
+            if (Object.prototype.hasOwnProperty.call(req.body, key)) {
+                if (key === "_id") continue; // no permitir cambio de _id
+                update[key] = req.body[key];
+            }
+        }
+        if (Object.keys(update).length === 0) {
+            return res.status(400).json({ message: "No update data provided" });
+        }
+        pagoSchema
+            .updateOne({ _id: id }, { $set: update })
+            .then((data) => {
+                // data contiene info sobre matchedCount / modifiedCount según la versión de mongoose
+                return res.json(data);
+            })
+            .catch((error) => res.status(500).json({ message: error }));
+    }
+);
