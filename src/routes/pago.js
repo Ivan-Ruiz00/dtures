ab12db9cc0df114f56fdfdd4c77c752265fe4785
Miguel Paez <luism.paezd@konradlorenz.edu.co>
2025-11-02 21:20:59 -0500

    Se agrega metodo para obtner pagos por id

--- src/routes/pago.js
+++ src/routes/pago.js
@@ -4,7 +4,7 @@ const pagoSchema = require("../models/pagoSchema");
 const verifyToken = require('./validate_token');
 
 router.post(
-    "/pago", verifyToken, (req, res) => {
+    "/pago", (req, res) => {
         let pago = new pagoSchema(req.body);
         pago.save()
             .then((data) => res.json(data))
@@ -13,10 +13,21 @@ router.post(
 );
 
 router.get(
-    "/pagos", verifyToken, (req, res) => {
+    "/pago", (req, res) => {
         pagoSchema.find()
             .then((data) => res.json(data))
             .catch((error) => res.json({ message: error }));
     }
 );
+
+router.get(
+    "/pago/:id", (req, res) => {
+        const { id } = req.params;
+        pagoSchema
+            .findById(id)
+            .then((data) => res.json(data))
+            .catch((error) => res.json({ message: error }));
+    }
+);
+
 module.exports = router;
